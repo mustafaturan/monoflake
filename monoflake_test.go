d55cbd363230123ab57f2fc4abeeb6e04a796d6b
Mustafa Turan <mustafaturan.net@gmail.com>
2024-06-09 19:06:57 -0700

    Enhance tests to cover all possible node bits

--- monoflake_test.go
+++ monoflake_test.go
@@ -56,27 +56,31 @@ func TestNew(t *testing.T) {
 }
 
 func TestNext(t *testing.T) {
-	tid, _ := New(100)
-	t.Run("generates greater sequences on each call until max sequence", func(t *testing.T) {
-		t.Parallel()
-		for i := 0; i < int(tid.maxSequence)*1000; i++ {
-			tid1, tid2 := tid.Next(), tid.Next()
-			if tid1 >= tid2 {
-				debug := map[string]int64{
-					"since":     time.Since(tid.epoch).Milliseconds(),
-					"max_seq":   tid.maxSequence,
-					"time1":     tid1.Since(),
-					"time2":     tid2.Since(),
-					"sequence1": tid1.Sequence(tid.nodeBits),
-					"sequence2": tid2.Sequence(tid.nodeBits),
-					"node_id1":  tid1.NodeID(tid.nodeBits),
-					"node_id2":  tid2.NodeID(tid.nodeBits),
-					"i":         int64(i),
+	// test for all possible node bits
+	for i := 8; i < 14; i++ {
+		tid, _ := New(100, WithNodeBits(i))
+		t.Run("generates greater sequences on each call until max sequence", func(t *testing.T) {
+			t.Parallel()
+			for i := 0; i < int(tid.maxSequence)*250; i++ {
+				tid1, tid2 := tid.Next(), tid.Next()
+				if tid1 >= tid2 {
+					debug := map[string]int64{
+						"since":     time.Since(tid.epoch).Milliseconds(),
+						"max_seq":   tid.maxSequence,
+						"time1":     tid1.Since(),
+						"time2":     tid2.Since(),
+						"sequence1": tid1.Sequence(tid.nodeBits),
+						"sequence2": tid2.Sequence(tid.nodeBits),
+						"node_id1":  tid1.NodeID(tid.nodeBits),
+						"node_id2":  tid2.NodeID(tid.nodeBits),
+						"i":         int64(i),
+					}
+					t.Errorf("Next(): %d >= Next(): %d %v", tid1, tid2, debug)
 				}
-				t.Errorf("Next(): %d >= Next(): %d %v", tid1, tid2, debug)
 			}
-		}
-	})
+		})
+	}
+
 }
 
 func TestErrorString(t *testing.T) {
