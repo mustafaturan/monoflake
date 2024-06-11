9c817efb2a44b0e3a865739f345125d372117373
Mustafa Turan <mustafaturan.net@gmail.com>
2024-06-10 22:33:09 -0700

    Add copyright information to the source code and fix year on the existing copyright

--- id.go
+++ id.go
@@ -1,3 +1,7 @@
+// Copyright 2024 Mustafa Turan. All rights reserved.
+// Use of this source code is governed by a Apache License 2.0 license that can
+// be found in the LICENSE file.
+
 package monoflake
 
 type ID int64
